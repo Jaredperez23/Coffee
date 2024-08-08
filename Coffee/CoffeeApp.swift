10eff00e8375117fe39d94267d9d1a57996a81d2
jaredperez23 <jaredperez5623@gmail.com>
2024-08-07 19:02:38 -0700

    Initial Commit

---
+++ Coffee/CoffeeApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoffeeApp.swift
+//  Coffee
+//
+//  Created by Jared Perez on 8/7/24.
+//
+
+import SwiftUI
+
+@main
+struct CoffeeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
